59cfc782328ab60b2992df0c434e8e1068fd1d42
Niko Kivelä <niko@tovrleaf.com>
2011-02-04 23:01:25 +0200

    start-egg.sh moved from branch os-configs

---
+++ start-egg.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# @author Niko Kivel <niko@tovrleaf.com>
+# @since Sun Aug 22 22:59:46 EEST 2010
+
+# Check that eggdrop is running and if not, start it again.
+# This script should be run with cron.
+
+# NOTE: this same functionality could be be achieved by using the
+# built-in script called `botchk`, see http://www.egghelp.org/setup.htm#config
+
+# every confname needs to be unique per system
+CONF='MyBotName.eggdrop.conf'
+DIR='/path/to/eggdrop/'
+
+if ps -ef | grep -v grep | grep $CONF ; then
+        exit 0
+else
+        cd $DIR
+        ./eggdrop $CONF &
+        exit 0
+fi
